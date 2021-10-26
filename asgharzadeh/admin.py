b14ff7678c075b1d221e5cfb60bde5354e56b652
Grace Hawkins <amoomajid99@gmail.com>
2021-10-26 15:27:01 +0330

    Edit admin page of night name

--- asgharzadeh/admin.py
+++ asgharzadeh/admin.py
@@ -2,5 +2,10 @@ from django.contrib import admin
 
 from . import models
 
+
 admin.site.register(models.Room)
-admin.site.register(models.NightName)
+
+
+@admin.register(models.NightName)
+class NightNameAdmin(admin.ModelAdmin):
+    fields = ('name', 'start', 'end')
